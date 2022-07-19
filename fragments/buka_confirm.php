8828dbd5ef812b276e147358c17bb91dc8ece287
Wolfgang Bund <wb@dtp-net.de>
2022-07-19 10:10:50 +0200

    Fragment Zusammenfassung für sprog vorbereitet
    Danke @Mike Lang

--- fragments/buka_confirm.php
+++ fragments/buka_confirm.php
@@ -3,15 +3,57 @@ $booking = $this->booking;
 $price = $this->price;
 $object = buka_objects::get_object_for_id($booking['object_id']);
 
+
+
+$str_summery			= 'Zusammenfassung Ihrer ';
+$str_booking 			= 'Buchung';
+$str_request 			= 'Anfrage';
+$str_object 			= 'Objekt';
+$str_arrival			= 'Anreise';
+$str_departure			= 'Abreise';
+$str_overnightstays	= 'Übernachtungen';
+$str_country			= 'Land';
+$str_tel					= 'Telefon';
+$str_email				= 'E-Mail';
+$str_no_persons		= 'Anzahl Personen';
+$str_arrival_time		= 'Anreisezeit';
+$str_price				= 'Preis';
+$str_agree_1 			= 'Mit dem Absenden dieser';
+$str_agree_2 			= 'erklären Sie sich mit unserer Hausordnung und AGB einverstanden.';
+
+// schön wäre wenn "Hausordnung und AGB" ein Link wäre 
+
+
+
+
+if (rex_addon::get('sprog')->isAvailable()) {
+	$str_summery			= sprogcard('buka_summery')			?: $str_summery;
+	$str_booking			= sprogcard('buka_booking')			?: $str_booking;
+	$str_request			= sprogcard('buka_request')			?: $str_request;
+	$str_object				= sprogcard('buka_object')				?: $str_object;
+	$str_arrival			= sprogcard('buka_arrival')			?: $str_arrival;
+	$str_departure			= sprogcard('buka_departure')			?: $str_departure;
+	$str_overnightstays	= sprogcard('buka_overnightstays')	?: $str_overnightstays;
+	$str_country			= sprogcard('buka_country')			?: $str_country;
+	$str_tel					= sprogcard('buka_tel')					?: $str_tel;
+	$str_email				= sprogcard('buka_email')				?: $str_email;
+	$str_no_persons		= sprogcard('buka_no_persons')		?: $str_no_persons;
+	$str_arrival_time		= sprogcard('buka_arrival_time')		?: $str_arrival_time;
+	$str_price				= sprogcard('buka_price')				?: $str_price;
+	$str_agree_1			= sprogcard('buka_agree_1')			?: $str_agree_1;
+	$str_agree_2			= sprogcard('buka_agree_2')			?: $str_agree_2;
+}
+        
+
 ?>
 
 <div class="alert alert-success">
 
-	<h2>Zusammenfassung Ihrer <?= ($object->reservation == 'book' ? 'Buchung' : 'Anfrage') ?></h2>
-	<p>Objekt: <?= $object->name ?></p>
-	<p>Anreise: <?= date('d.m.Y', strtotime($booking['datestart'])) ?></p>
-	<p>Abreise: <?= date('d.m.Y', strtotime($booking['dateend'])) ?></p>
-	<p>Übernachtungen: <?= count($price['period'])  ?></p>
+	<h2><?= $str_summery	.' '.($object->reservation == 'book' ? $str_booking : $str_request) ?></h2>
+	<p><?= $str_object	.': '.$object->name ?></p>
+	<p><?= $str_arrival	.': '.date('d.m.Y', strtotime($booking['datestart'])) ?></p>
+	<p><?= $str_departure.': '.date('d.m.Y', strtotime($booking['dateend'])) ?></p>
+	<p><?= $str_overnightstays.': '.count($price['period'])  ?></p>
 	<p>
 		<?php if ($booking['vorname'] . $booking['nachname']) : ?>
 			<?= $booking['vorname'] . ' ' . $booking['nachname'] ?><br>
@@ -23,22 +65,23 @@ $object = buka_objects::get_object_for_id($booking['object_id']);
 			<?= $booking['plz'] . ' ' . $booking['ort'] ?><br>
 		<?php endif ?>
 		<?php if ($booking['land']) : ?>
-			Land <?= $booking['land'] ?><br>
+			<?= $str_country.': '.$booking['land'] ?><br>
 		<?php endif ?>
 		<?php if ($booking['telefon']) : ?>
-			Telefon <?= $booking['telefon'] ?><br>
+			<?= $str_tel.': '.$booking['telefon'] ?><br>
 		<?php endif ?>
 		<?php if ($booking['email']) : ?>
-			E-Mail <?= $booking['email'] ?>
+			<?= $str_email.': '.$booking['email'] ?>
 		<?php endif ?>
 	</p>
 	<?php if ($booking['personen']) : ?>
-		<p>Anzahl Personen: <?= $booking['personen'] ?></p>
+		<p><?= $str_no_persons.': '.$booking['personen'] ?></p>
 	<?php endif ?>
 	<?php if ($booking['anreisezeit']) : ?>
-		<p>Anreisezeit: <?= $booking['anreisezeit'] ?></p>
+		<p><?= $str_arrival_time.': '.$booking['anreisezeit'] ?></p>
 	<?php endif ?>
-	<p>Preis: <?= number_format($price['price'], 2, ',', "'") ?> EUR</p>
-	<p>Mit dem Absenden dieser <?= ($object->reservation == 'book' ? 'Buchung' : 'Anfrage') ?> erklären Sie sich mit unserer Hausordnung und AGB einverstanden.</p>
+	<p><?= $str_price.': '.number_format($price['price'], 2, ',', ".") ?> EUR</p>
+	<p><?= $str_price_warn ?></p>
+	<p><?= $str_agree_1.' '.($object->reservation == 'book' ? $str_booking : $str_request).' '.$str_agree_2 ?> </p>
 
 </div>
